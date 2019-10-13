10630420d9c9e3f612adb4df7765f79d913a8207
Tim van Deurzen <tim@kompiler.org>
2019-10-14 00:03:02 +0200

    Add backward compatibility check for SDL2 target.

--- demo/demo.cmake
+++ demo/demo.cmake
@@ -1,4 +1,14 @@
 if(sdl2_FOUND)
+  # Create the target if it isn't there, this can happen on a system that has a
+  # very simple (old) find-module for SDL2.
+  if (NOT TARGET SDL2::SDL2)
+    add_library(SDL2::SDL2 INTERFACE IMPORTED)
+    set_target_properties(SDL2::SDL2 PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${SDL2_INCLUDE_DIRS}"
+      INTERFACE_LINK_LIBRARIES "${SDL2_LIBRARIES}"
+      )
+  endif()
+
 
   add_executable(CsPaintDemo
     demo/data.cpp
